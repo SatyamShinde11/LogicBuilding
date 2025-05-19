b232158393d3418f76f7276b60432d1631f737ff
user.name "Satyam Shinde <user.email "satyamshinde301@gmail.com>
2025-05-19 08:45:38 +0530

    solved new example

---
+++ Day1.js
@@ -0,0 +1,31 @@
+// Problem NO 1
+
+// console.log("Hello Word")
+
+// Problem NO 2
+
+// function Sum(a, b) {
+//     let sum = a + b
+//     console.log("Sum is " + sum);
+
+// }
+// Sum(2, 7)
+
+// Problem NO 3
+
+function fideRectangleArea(length, width) {
+
+    if (length <= 0 || width <= 0) {
+        return console.log("your value is wrong")
+    }
+
+    const a = length * width
+    console.log("Rectangle Area is " + a)
+
+
+
+}
+
+fideRectangleArea(100, 200)
+fideRectangleArea(100, -10)
+fideRectangleArea(-20, 10)
